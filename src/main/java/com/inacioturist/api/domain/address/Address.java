bec5cf353117b8938963b2ead85e37001f489438
inaciogabriell <gabriel.inaciofn@gmail.com>
2024-07-24 00:36:32 -0300

    Creating entities

---
+++ src/main/java/com/inacioturist/api/domain/address/Address.java
@@ -0,0 +1,24 @@
+package com.inacioturist.api.domain.address;
+
+
+import com.inacioturist.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
