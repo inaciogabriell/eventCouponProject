9c31825ff2ae2f9138b6034e10d4aa22895f7f7b
inaciogabriell <gabriel.inaciofn@gmail.com>
2024-08-23 19:50:16 -0300

    coupon system finished

---
+++ src/main/java/com/inacioturist/api/domain/coupon/CouponRequestDTO.java
@@ -0,0 +1,4 @@
+package com.inacioturist.api.domain.coupon;
+
+public record CouponRequestDTO(String code, Integer discount, Long valid) {
+}
--- src/main/java/com/inacioturist/api/entities/CouponRepository.java
+++ src/main/java/com/inacioturist/api/entities/CouponRepository.java
@@ -3,7 +3,10 @@ package com.inacioturist.api.entities;
 import com.inacioturist.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, Date currentDate);
 }
